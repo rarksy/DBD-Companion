e7792d824e81177bc2606267f5c0cf46cd552de9
Rarksy <corrip2004@gmail.com>
2022-07-26 15:57:08 +1000

    removed redundant classes

--- Dead By Daylight Companion/Hook Counter/Hook Counter.cs
+++ Dead By Daylight Companion/Hook Counter/Hook Counter.cs
@@ -2,16 +2,9 @@
 using OpenCvSharp.Extensions;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Dead_By_Daylight_Companion.Hook_Counter {
