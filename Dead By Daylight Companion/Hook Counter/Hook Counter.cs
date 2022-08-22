0734d026c28003398318228e0b4576c72eda4481
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-08-22 00:15:14 -0400

    Don't read templates from the disk every tick.

--- Dead By Daylight Companion/Hook Counter/Hook Counter.cs
+++ Dead By Daylight Companion/Hook Counter/Hook Counter.cs
@@ -10,19 +10,32 @@ using System.Windows.Forms;
 
 namespace Dead_By_Daylight_Companion.Hook_Counter {
     public partial class Hook_Counter : Form {
-        public Hook_Counter() => InitializeComponent();
+        public Hook_Counter() {
+            Disposed += Hook_Counter_Disposed;
+            InitializeComponent();
+        }
+
+        private void Hook_Counter_Disposed(object sender, EventArgs e) {
+            Frame.Dispose();
+            Hook.Dispose();
+            Stage2.Dispose();
+            EndGame.Dispose();
+        }
+
         static overlay ov = new overlay();
-        public static bool bInitOverlay = false, bCheckEndGame = true;
-        public static string res = Screen.PrimaryScreen.Bounds.Width + Screen.PrimaryScreen.Bounds.Height.ToString(),
+        public static bool bInitOverlay = false;
+        public static string res = Screen.PrimaryScreen.Bounds.Width.ToString() + Screen.PrimaryScreen.Bounds.Height.ToString(),
                              CurFontName = Properties.Settings.Default.FontName,
                              HookText;
         public static int CurFontSize = Properties.Settings.Default.FontSize;
         public static List<string> hCount = new List<string>();
         public static List<string> _2stage = new List<string>();
+        private Mat Frame;
+        private Mat Hook, Stage2, EndGame;
 
         [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")] public extern static void ReleaseCapture();
         [DllImport("user32.DLL", EntryPoint = "SendMessage")] public extern static void SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
-        
+
         private void ExitHookCounter_Click(object sender, EventArgs e) {
             Environment.Exit(0);
         }
@@ -69,58 +82,14 @@ namespace Dead_By_Daylight_Companion.Hook_Counter {
                 Properties.Settings.Default.Save();
             }
         }
-        
-        private Bitmap ITM(Mat source, string template, double dthreshold, ref List<string> count) {
-            using (Mat tempMat = new Mat(template))
-            using (Mat newMat = new Mat(source.Rows - tempMat.Rows + 1, source.Cols - tempMat.Cols + 1, MatType.CV_32FC1)) {
-                Mat sourceGRAY = source.CvtColor(ColorConversionCodes.BGR2GRAY);
-                Mat tempGRAY = tempMat.CvtColor(ColorConversionCodes.BGR2GRAY);
-                Cv2.MatchTemplate(sourceGRAY, tempGRAY, newMat, TemplateMatchModes.CCoeffNormed);
-                sourceGRAY.Dispose();
-                tempGRAY.Dispose();
-                Cv2.Threshold(newMat, newMat, dthreshold, 1.0, ThresholdTypes.Tozero);
 
-                while (true) {
-                    double mnval, mxval;
-                    OpenCvSharp.Point mnloc, mxloc;
-                    Cv2.MinMaxLoc(newMat, out mnval, out mxval, out mnloc, out mxloc);
-
-                    if (mxval >= dthreshold) {
-                        if (template.Contains("endgame") && bCheckEndGame) {
-                            if (overlay.G != null) overlay.G.Clear(Color.Black);
-                            overlay.sList.Clear();
-                            overlay._2List.Clear();
-                            _2stage.Clear();
-                            hCount.Clear();
-                            break;
-                        }
-                        count.Add(mxloc.Y.ToString());
-                        Rect r = new Rect(new OpenCvSharp.Point(mxloc.X, mxloc.Y), new OpenCvSharp.Size(tempMat.Width, tempMat.Height));
-                        Rect outRect;
-                        Cv2.FloodFill(newMat, mxloc, new Scalar(0), out outRect, new Scalar(0.1), new Scalar(1.0));
-                        overlay.bHasDrawn = false;
-                    }
-                    else
-                        break;
-                }
-                newMat.Dispose();
-                tempMat.Dispose();
-
-                return source.ToBitmap();
+        private bool CompareToTemplate(Mat source, Mat template, double threshold, out OpenCvSharp.Point maxLoc) {
+            using (var result = new Mat(source.Rows - template.Rows + 1, source.Cols - template.Cols + 1, MatType.CV_32FC1)) {
+                Cv2.MatchTemplate(source, template, result, TemplateMatchModes.CCoeffNormed);
+                Cv2.Threshold(result, result, threshold, 1.0, ThresholdTypes.Tozero);
+                Cv2.MinMaxLoc(result, out _, out double maxVal, out _, out maxLoc);
+                return maxVal >= threshold;
             }
-        }
-        private Bitmap GetFrame() {
-            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width / 5, Screen.PrimaryScreen.Bounds.Height);
-            using (var g = Graphics.FromImage(bmp)) {
-                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X / 5,
-                        Screen.PrimaryScreen.Bounds.Y,
-                        0,
-                        0,
-                        Screen.PrimaryScreen.Bounds.Size,
-                        CopyPixelOperation.SourceCopy);
-                g.Dispose();
-            }
-            return bmp;
         }
 
         private void TitlePanel_MouseDown(object sender, MouseEventArgs e) {
@@ -131,15 +100,17 @@ namespace Dead_By_Daylight_Companion.Hook_Counter {
         private void TitleLabel_MouseDown(object sender, MouseEventArgs e) {
             TitlePanel_MouseDown(sender, e);
         }
-        
+
         private void UIScale_SelectedIndexChanged(object sender, EventArgs e) {
             Properties.Settings.Default.UIScale = UIScale.SelectedIndex;
             Properties.Settings.Default.Save();
+            ReloadRes();
         }
-        
+
         private void IGUIScale_SelectedIndexChanged(object sender, EventArgs e) {
             Properties.Settings.Default.IGUIScale = IGUIScale.SelectedIndex;
             Properties.Settings.Default.Save();
+            ReloadRes();
         }
 
         private void HookTextBox_TextChanged(object sender, EventArgs e) {
@@ -147,12 +118,28 @@ namespace Dead_By_Daylight_Companion.Hook_Counter {
             Properties.Settings.Default.HookedText = HookText;
         }
 
+        private void ReloadRes() {
+            if (!string.IsNullOrEmpty(IGUIScale.Text)) {
+                using (var h = new Mat($@"resources\{res}\hook{IGUIScale.Text}.png"))
+                    Hook = h.CvtColor(ColorConversionCodes.BGR2GRAY);
+
+                using (var s2 = new Mat($@"resources\{res}\2stage{IGUIScale.Text}.png"))
+                    Stage2 = s2.CvtColor(ColorConversionCodes.BGR2GRAY);
+            }
+
+            if (!string.IsNullOrEmpty(UIScale.Text)) {
+                using (var eg = new Mat($@"resources\{res}\endgame{UIScale.Text}.png"))
+                    EndGame = eg.CvtColor(ColorConversionCodes.BGR2GRAY);
+            }
+        }
+
         private void CountStageCB_CheckedChanged(object sender, EventArgs e) {
             if (CountStageCB.Checked) {
                 if (!File.Exists($@"resources\{res}\2stage100.png")) {
                     CountStageCB.Checked = false;
                     MessageBox.Show("Second Stage Resources Not Found, Disabling");
-                } else {
+                }
+                else {
                     Properties.Settings.Default.CountStages = true;
                     HookTextBox.Enabled = false;
                     HookText = "I";
@@ -166,14 +153,14 @@ namespace Dead_By_Daylight_Companion.Hook_Counter {
             Properties.Settings.Default.Save();
         }
 
-            private void Hook_Counter_Load(object sender, EventArgs e) {
-            UIScale.SelectedIndex = Properties.Settings.Default.UIScale; 
+        private void Hook_Counter_Load(object sender, EventArgs e) {
+            UIScale.SelectedIndex = Properties.Settings.Default.UIScale;
             IGUIScale.SelectedIndex = Properties.Settings.Default.IGUIScale;
             HookTextBox.Text = Properties.Settings.Default.HookedText;
             CountStageCB.Checked = Properties.Settings.Default.CountStages;
             FontLabel.Text = $"Font: {CurFontName}";
             FontSizeLabel.Text = $"Font Size: {CurFontSize}";
-            
+
             Thread.Start();
         }
 
@@ -183,19 +170,43 @@ namespace Dead_By_Daylight_Companion.Hook_Counter {
                 ov.Show();
                 bInitOverlay = true;
             }
-            Bitmap frame = GetFrame();
-            Mat mat = BitmapConverter.ToMat(frame);
-            Bitmap det_first = ITM(mat, $@"resources\{res}\hook{IGUIScale.Text}.png", LowerThreshCheckbox.Checked ? 0.8 : 0.9, ref hCount);
-            if (CountStageCB.Checked) {
-                Bitmap det_second = ITM(mat, $@"resources\{res}\2stage{IGUIScale.Text}.png", 0.9, ref _2stage);
-                det_second.Dispose();
-            }
-            Bitmap det_endgame = ITM(mat, $@"resources\{res}\endgame{UIScale.Text}.png", 0.9, ref hCount);
 
-            frame.Dispose();
-            mat.Dispose();
-            det_first.Dispose();
-            det_endgame.Dispose();
+            // Grab the screen.
+            // TODO: Grab just DBD, not the whole screen.
+            Frame?.Dispose();
+            using (var b = new Bitmap(Screen.PrimaryScreen.Bounds.Width / 5, Screen.PrimaryScreen.Bounds.Height))
+            using (var g = Graphics.FromImage(b)) {
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X / 5,
+                        Screen.PrimaryScreen.Bounds.Y,
+                        0,
+                        0,
+                        Screen.PrimaryScreen.Bounds.Size,
+                        CopyPixelOperation.SourceCopy);
+                using (var m = BitmapConverter.ToMat(b)) {
+                    Frame = m.CvtColor(ColorConversionCodes.BGR2GRAY);
+                }
+            }
+
+            // Check for first hook.
+            if (CompareToTemplate(Frame, Hook, LowerThreshCheckbox.Checked ? 0.8 : 0.9, out OpenCvSharp.Point hookLoc)) {
+                hCount.Add(hookLoc.Y.ToString());
+                overlay.bHasDrawn = false;
+            }
+
+            // Check for second stage.
+            if (CountStageCB.Checked && CompareToTemplate(Frame, Stage2, 0.9, out OpenCvSharp.Point stage2Loc)) {
+                _2stage.Add(stage2Loc.Y.ToString());
+                overlay.bHasDrawn = false;
+            }
+
+            // Check for endgame.
+            if (CompareToTemplate(Frame, EndGame, 0.9, out OpenCvSharp.Point _)) {
+                overlay.G?.Clear(Color.Black);
+                overlay.sList.Clear();
+                overlay._2List.Clear();
+                _2stage.Clear();
+                hCount.Clear();
+            }
         }
     }
 }
